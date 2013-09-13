2f67fc6b797c0b7a2f4a1f1d19ca5900060899e5
visaldattabobba <visaldattabobba@gmail.com>
2013-09-12 22:29:48 -0400

    commit1
    sample

---
+++ sample/sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey wats up!");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
